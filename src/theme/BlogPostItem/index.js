b493660416f0305b8dd38dab29b1d6c82794db59
sqybi <sqybi@126.com>
2023-11-24 00:38:21 +0800

    Fix giscus: correct color mode, do not show in blog list, disable by frontmatter

--- src/theme/BlogPostItem/index.js
+++ src/theme/BlogPostItem/index.js
@@ -2,6 +2,8 @@ import React from 'react';
 import BlogPostItem from '@theme-original/BlogPostItem';
 import Giscus from '@giscus/react';
 import useDocusaurusContext from '@docusaurus/useDocusaurusContext';
+import { useColorMode } from '@docusaurus/theme-common';
+import { useBlogPost } from '@docusaurus/theme-common/internal';
 
 const localeMap = {
   'en-US': 'en',
@@ -10,29 +12,40 @@ const localeMap = {
 
 export default function BlogPostItemWrapper(props) {
   const {
-    siteConfig: {customFields},
-    i18n: {currentLocale},
+    siteConfig: { customFields },
+    i18n: { currentLocale },
   } = useDocusaurusContext();
+  const {
+    colorMode
+  } = useColorMode();
+  const {
+    metadata,
+    isBlogPostPage
+  } = useBlogPost();
+  const { frontMatter } = metadata;
+  const { disable_comments: disableComments } = frontMatter
   return (
     <>
       <BlogPostItem {...props} />
-      <Giscus
-        id="comments"
-        repo="sqybi/SQYBI.com"
-        repoId={customFields.giscusRepoId}
-        category="Announcements"
-        categoryId={customFields.giscusCategoryId}
-        mapping="pathname"
-        strict="1"
-        reactionsEnabled="1"
-        emitMetadata="0"
-        inputPosition="top"
-        theme="preferred_color_scheme"
-        lang={localeMap[currentLocale] ?? currentLocale}
-        loading="lazy"
-        crossorigin="anonymous"
-        async
-      />
+      {(!disableComments && isBlogPostPage) && (
+        <Giscus
+          id="comments"
+          repo="sqybi/SQYBI.com"
+          repoId={customFields.giscusRepoId}
+          category="Announcements"
+          categoryId={customFields.giscusCategoryId}
+          mapping="pathname"
+          strict="1"
+          reactionsEnabled="1"
+          emitMetadata="0"
+          inputPosition="top"
+          theme={colorMode}
+          lang={localeMap[currentLocale] ?? currentLocale}
+          loading="lazy"
+          crossorigin="anonymous"
+          async
+        />
+      )}
     </>
   );
 }
