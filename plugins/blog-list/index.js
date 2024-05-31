f13da82251198bf2cdd19f824b27bdfe7a4ba9e8
sqybi <sqybi@126.com>
2024-06-01 01:45:45 +0800

    Fix plugin

--- plugins/blog-list/index.js
+++ plugins/blog-list/index.js
@@ -4,11 +4,17 @@ import path from 'path';
 export default async function blogList(context, options) {
     return {
         name: 'blog-list',
-        async loadContent() {
+        async loadContent() {},
+        async contentLoaded({ content, actions }) {
             const blogDir = path.join(__dirname, '../../.docusaurus/docusaurus-plugin-content-blog/default');
             const blogs = {
                 "items": [],
             };
+            // Wait for the blog metadata to be generated
+            while (!fs.existsSync(blogDir)) {
+                await new Promise(resolve => setTimeout(resolve, 1000));
+            }
+            await new Promise(resolve => setTimeout(resolve, 5000));  // Wait another 5 seconds to make sure the metadata is fully generated
             for (const file of fs.readdirSync(blogDir)) {
                 if (file.startsWith('site-blog-') && file.endsWith('.json')) {
                     const metadata = JSON.parse(fs.readFileSync(path.join(blogDir, file), 'utf-8'));
@@ -16,13 +22,10 @@ export default async function blogList(context, options) {
                 }
             }
             blogs.items.sort((a, b) => a.date < b.date ? 1 : (a.date == b.date ? 0 : -1));
-            return blogs;
-        },
-        async contentLoaded({ content, actions }) {
             const { createData } = actions;
             const filePath = await createData(
                 'blog-post-list-full.json',
-                JSON.stringify(content, null, 2)
+                JSON.stringify(blogs, null, 2)
             );
             return filePath;
         },
