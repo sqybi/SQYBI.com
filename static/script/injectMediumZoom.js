eef1189af6d14e909d33a9d674092511f3d2ada2
sqybi <sqybi@126.com>
2024-11-12 01:26:54 +0800

    Make mediumZoom always work -- it works perfect!

--- static/script/injectMediumZoom.js
+++ static/script/injectMediumZoom.js
@@ -1,9 +1,29 @@
-window.onload = () => {
-    setTimeout(() => {
-        mediumZoom('[data-zoomable]', {
-            margin: 24,
-            background: '#000000cc',
-            scrollOffset: 40,
-        });
-    }, 1000);
-};
+function handleNewImage(imgElement) {
+    if (!imgElement.hasAttribute('data-zoomable')) {
+        return;
+    }
+    if (imgElement.classList.contains('medium-zoom-image')) {
+        return;
+    }
+    mediumZoom(imgElement, {
+        margin: 24,
+        background: '#000000cc',
+        scrollOffset: 40,
+    });
+}
+
+const observer = new MutationObserver((mutationsList) => {
+    mutationsList.forEach((mutation) => {
+        if (mutation.type === 'childList') {
+            mutation.addedNodes.forEach((node) => {
+                if (node.tagName === 'IMG') {
+                    handleNewImage(node);
+                } else if (node.nodeType === Node.ELEMENT_NODE) {
+                    node.querySelectorAll('img').forEach(handleNewImage);
+                }
+            });
+        }
+    });
+});
+
+observer.observe(document.body, { childList: true, subtree: true });
